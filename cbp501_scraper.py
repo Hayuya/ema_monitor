5c9e8941a460a353c89455035af7b9b3b3f4ea0e
hayuya <e3131901@foreign.kitakyu-u.ac.jp>
2025-09-05 21:34:04 +0900

    Sc Ev Ne

--- cbp501_scraper.py
+++ cbp501_scraper.py
@@ -18,7 +18,7 @@ class CBP501Scraper:
         """初期化"""
         self.base_urls = [
             'https://www.ema.europa.eu/en/news',
-            'https://www.ema.europa.eu/en/human-regulatory/research-and-development/clinical-trials-human-medicines'
+            'https://www.ema.europa.eu/en/events/upcoming-events'
         ]
         self.headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
